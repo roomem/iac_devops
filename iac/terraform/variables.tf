385b7c12a411ee8457919eb6f70ae109932e967f
Rome <romegioli.work@gmail.com>
2023-12-04 17:54:17 +0100

    variables add appid password definition

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,3 +1,13 @@
 variable "resource_group_name" {
   type = string
 }
+
+variable "appId" {
+  type = string
+  description = "Azure Application ID"
+}
+
+variable "password" {
+  type = string
+  description = "Azure application password"
+}
