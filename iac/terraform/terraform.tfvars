d86cbfafb7b0832d8f1d8161568b980bc1ad075f
Rome <romegioli.work@gmail.com>
2023-12-05 10:48:30 +0100

    vnet setting, vars added, nsg and cluster to do

--- iac/terraform/terraform.tfvars
+++ iac/terraform/terraform.tfvars
@@ -1,1 +1,8 @@
 resource_group_name = "BU-MT"
+default_location = "westeurope"
+
+vnet_name           = "rome_vnet"
+address_space       = ["10.0.0.0/16"]
+subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
+
+tag = "rome"
--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -2,6 +2,10 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "default_location" {
+  type = string
+}
+
 variable "appId" {
   type = string
   description = "Azure Application ID"
@@ -10,4 +14,21 @@ variable "appId" {
 variable "password" {
   type = string
   description = "Azure application password"
+}
+
+variable "vnet_name" {
+  description = "Nome della Virtual Network"
+}
+
+variable "address_space" {
+  description = "Spazio degli indirizzi della VNet"
+}
+
+variable "subnet_prefixes" {
+  description = "Prefissi delle subnet"
+}
+
+variable "tag" {
+  type = string
+  description = "tag del progetto"
 }
