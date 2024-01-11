ac598d8d4122c3b83379c0e6f7b8a35cb983a2d0
Rome <romegioli.work@gmail.com>
2024-01-11 10:31:20 +0100

    fix

--- iac/terraform1/demo_bucket_add.tf
+++ iac/terraform1/demo_bucket_add.tf
@@ -1,15 +1,15 @@
 resource "azurerm_storage_account" "example" {
-  name = "my-storage-account"
+  name = "storage"
   location = var.default_location
   resource_group_name = var.resource_group_name
 
-  account_tier = "Standard_LRS"
+  account_tier = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "example" {
-  name = "azure_bucket_example"
-  storage_account_name = "example"
+  name = "bucket"
+  storage_account_name = azurerm_storage_account.example.name
 
   container_access_type = "private"
 }
